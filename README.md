78fed2a72885e7935c0c59f8966960d491aef265
Tony Alejandro <tonysviews@gmail.com>
2019-11-30 15:29:22 +0800

    changed component tag to mv-lnr instead of mv-linear-icon refactor demo to use new component tag mv-lnr updated README.md

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
  MvLinearIcon is a Meveo icon component (based on lit-element) that renders [Linearicons Free](https://linearicons.com/free) library icons.
 
 ## Features
-* Written in vanila javascript
+* Written in vanilla javascript
 * Simplifies icon loading from the library using only the icon name without the prefix
 
 
@@ -19,12 +19,12 @@ To experiment with the MvLinearIcon component.
     <style>
       @font-face {
         font-family: 'Linearicons-Free';
-        src: url('./fonts/Linearicons-Free.eot?w118d');
-        src: url('./fonts/Linearicons-Free.eot?#iefixw118d') format('embedded-opentype'),
-          url('./fonts/Linearicons-Free.woff2?w118d') format('woff2'),
-          url('./fonts/Linearicons-Free.woff?w118d') format('woff'),
-          url('./fonts/Linearicons-Free.ttf?w118d') format('truetype'),
-          url('./fonts/Linearicons-Free.svg?w118d#Linearicons-Free') format('svg');
+        src: url('{REPLACE_WITH_PATH_TO}/fonts/Linearicons-Free.eot?w118d');
+        src: url('{REPLACE_WITH_PATH_TO}/fonts/Linearicons-Free.eot?#iefixw118d') format('embedded-opentype'),
+          url('{REPLACE_WITH_PATH_TO}/fonts/Linearicons-Free.woff2?w118d') format('woff2'),
+          url('{REPLACE_WITH_PATH_TO}/fonts/Linearicons-Free.woff?w118d') format('woff'),
+          url('{REPLACE_WITH_PATH_TO}/fonts/Linearicons-Free.ttf?w118d') format('truetype'),
+          url('{REPLACE_WITH_PATH_TO}/fonts/Linearicons-Free.svg?w118d#Linearicons-Free') format('svg');
         font-weight: normal;
         font-style: normal;
       }
@@ -34,7 +34,7 @@ To experiment with the MvLinearIcon component.
 4. View the index.html to see available icons, then use the component to load the chosen icon e.g.
 
    ```html
-   <mv-linear-icon icon="home"></mv-linear-icon>
+   <mv-lnr icon="home"></mv-lnr>
    ```
 
 ## Acknowledgements
--- mv-linear-icon.js
+++ mv-linear-icon.js
@@ -1,9 +1,183 @@
 import { LitElement, html, css } from "lit-element";
 
+export const ICONS = [
+  "home",
+  "apartment",
+  "pencil",
+  "magic-wand",
+  "drop",
+  "lighter",
+  "poop",
+  "sun",
+  "moon",
+  "cloud",
+  "cloud-upload",
+  "cloud-download",
+  "cloud-sync",
+  "cloud-check",
+  "database",
+  "lock",
+  "cog",
+  "trash",
+  "dice",
+  "heart",
+  "star",
+  "star-half",
+  "star-empty",
+  "flag",
+  "envelope",
+  "paperclip",
+  "inbox",
+  "eye",
+  "printer",
+  "file-empty",
+  "file-add",
+  "enter",
+  "exit",
+  "graduation-hat",
+  "license",
+  "music-note",
+  "film-play",
+  "camera-video",
+  "camera",
+  "picture",
+  "book",
+  "bookmark",
+  "user",
+  "users",
+  "shirt",
+  "store",
+  "cart",
+  "tag",
+  "phone-handset",
+  "phone",
+  "pushpin",
+  "map-marker",
+  "map",
+  "location",
+  "calendar-full",
+  "keyboard",
+  "spell-check",
+  "screen",
+  "smartphone",
+  "tablet",
+  "laptop",
+  "laptop-phone",
+  "power-switch",
+  "bubble",
+  "heart-pulse",
+  "construction",
+  "pie-chart",
+  "chart-bars",
+  "gift",
+  "diamond",
+  "linearicons",
+  "dinner",
+  "coffee-cup",
+  "leaf",
+  "paw",
+  "rocket",
+  "briefcase",
+  "bus",
+  "car",
+  "train",
+  "bicycle",
+  "wheelchair",
+  "select",
+  "earth",
+  "smile",
+  "sad",
+  "neutral",
+  "mustache",
+  "alarm",
+  "bullhorn",
+  "volume-high",
+  "volume-medium",
+  "volume-low",
+  "volume",
+  "mic",
+  "hourglass",
+  "undo",
+  "redo",
+  "sync",
+  "history",
+  "clock",
+  "download",
+  "upload",
+  "enter-down",
+  "exit-up",
+  "bug",
+  "code",
+  "link",
+  "unlink",
+  "thumbs-up",
+  "thumbs-down",
+  "magnifier",
+  "cross",
+  "menu",
+  "list",
+  "chevron-up",
+  "chevron-down",
+  "chevron-left",
+  "chevron-right",
+  "arrow-up",
+  "arrow-down",
+  "arrow-left",
+  "arrow-right",
+  "move",
+  "warning",
+  "question-circle",
+  "menu-circle",
+  "checkmark-circle",
+  "cross-circle",
+  "plus-circle",
+  "circle-minus",
+  "arrow-up-circle",
+  "arrow-down-circle",
+  "arrow-left-circle",
+  "arrow-right-circle",
+  "chevron-up-circle",
+  "chevron-down-circle",
+  "chevron-left-circle",
+  "chevron-right-circle",
+  "crop",
+  "frame-expand",
+  "frame-contract",
+  "layers",
+  "funnel",
+  "text-format",
+  "text-format-remove",
+  "text-size",
+  "bold",
+  "italic",
+  "underline",
+  "strikethrough",
+  "highlight",
+  "text-align-left",
+  "text-align-center",
+  "text-align-right",
+  "text-align-justify",
+  "line-spacing",
+  "indent-increase",
+  "indent-decrease",
+  "pilcrow",
+  "direction-ltr",
+  "direction-rtl",
+  "page-break",
+  "sort-alpha-asc",
+  "sort-amount-asc",
+  "hand",
+  "pointer-up",
+  "pointer-right",
+  "pointer-down",
+  "pointer-left"
+];
+
 export class MvLinearIcon extends LitElement {
   static get properties() {
     return {
-      icon: { type: String, attribute: true }
+      icon: { type: String, attribute: true },
+      showIcon: { type: Boolean, attribute: false, reflect: true }
     };
   }
 
@@ -536,11 +710,23 @@ export class MvLinearIcon extends LitElement {
     `;
   }
 
+  constructor() {
+    super();
+    this.showIcon = false;
+  }
+
   render() {
-    return html`
-      <i class="lnr lnr-${this.icon}"></i>
-    `;
+    return this.showIcon
+      ? html`
+          <i class="lnr lnr-${this.icon}"></i>
+        `
+      : html``;
+  }
+
+  connectedCallback() {
+    this.showIcon = ICONS.includes(this.icon);
+    super.connectedCallback();
   }
 }
 
-customElements.define("mv-linear-icon", MvLinearIcon);
+customElements.define("mv-lnr", MvLinearIcon);
