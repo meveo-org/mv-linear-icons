a8c37dae41e95afedd949169978b0ea069fff581
nam.nhat <nam.nhat@manaty.net>
2020-02-24 22:10:50 +0700

    - update icon - add theme

--- demo.js
+++ demo.js
@@ -5,7 +5,8 @@ import { ICONS } from "./mv-linear-icons.js";
 export class MvLinearIconDemo extends LitElement {
   static get properties() {
     return {
-      value: { type: String, attribute: true }
+      value: { type: String, attribute: true },
+      open: { type: Boolean, attribute: true }
     };
   }
 
@@ -24,17 +25,21 @@ export class MvLinearIconDemo extends LitElement {
         float: left;
         overflow: hidden;
       }
+      
       .mls {
         margin-left: .25em;
       }
+      
       .clearfix:before,
       .clearfix:after {
         content: " ";
         display: table;
       }
+      
       .clearfix:after {
         clear: both;
       }
+      
       .fs1 {
         font-size: 20px;
       }
@@ -44,18 +49,40 @@ export class MvLinearIconDemo extends LitElement {
         font-size: 24px;
         color: #2196F3;
       }
+      
+      .theme {
+        display: flex;
+        justify-content: flex-start;
+      }
+      
+      .theme mv-lnr {
+        font-size: 50px;
+        cursor: pointer;
+        margin: 20px;
+      }
     `;
   }
 
+  constructor() {
+    super();
+    this.open = true;
+  }
+
   render() {
+    const iconColor = `color: ${this.open ? "yellow" : ""}`;
+    const backgroundColor = `background-color: ${this.open ? "" : "#373E48"}`;
+    const color = `color: ${this.open ? "" : "#FFFFFF"}`;
     return html`
-      <div class="clearfix">
+      <div class="clearfix" style="${backgroundColor}">
+        <div class="theme">
+          <mv-lnr icon="sun" style="${iconColor}" @click=${this.toggleSun}></mv-lnr>
+        </div>
       ${ICONS.map(
         icon => html`
         <div class="glyph fs1">
           <div class="clearfix">
-            <mv-lnr icon="${icon}"></mv-lnr>
-            <span class="mls"> ${icon}</span>
+            <mv-lnr icon="${icon}" style="${color}"></mv-lnr>
+            <span class="mls" style="${color}"> ${icon}</span>
           </div>
         </div>
         `
@@ -63,6 +90,15 @@ export class MvLinearIconDemo extends LitElement {
       </div>    
     `;
   }
+
+  toggleSun = () => {
+    this.open = !this.open;
+    if (this.open) {
+      this.theme = "light";
+    } else {
+      this.theme = "dark";
+    }
+  };
 }
 
 customElements.define("mv-linear-icons-demo", MvLinearIconDemo);
